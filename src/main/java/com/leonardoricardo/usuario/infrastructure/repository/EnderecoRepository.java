7177f9b15712a52c9ef4e150b58fee7055abf9e7
leonardoricardodev98 <leonardoricardo2087@gmail.com>
2026-05-04 21:05:20 -0300

    Adicionei as classes de security

---
+++ src/main/java/com/leonardoricardo/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,10 @@
+package com.leonardoricardo.usuario.infrastructure.repository;
+
+import com.leonardo.praticandospring.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
